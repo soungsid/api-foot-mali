2d9ded5488f91a3f79a4908cade86e9e38eff2d0
soungsid <soungsid@gmail.com>
2023-01-07 11:12:44 -0500

    Creation de la repository git

---
+++ src/main/java/com/footmali/apifootmali/ApiFootMaliApplication.java
@@ -0,0 +1,13 @@
+package com.footmali.apifootmali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFootMaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFootMaliApplication.class, args);
+	}
+
+}
